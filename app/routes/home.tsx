1fde494306ec2b6d0eb2eada2c476140e4a594d8
zGIKS <166082577+zGIKS@users.noreply.github.com>
2026-01-12 13:24:51 -0500

    chore: rename project to "pretty-webapp" and update favicon assets
    - Changed project name in package.json from "--typescript" to "pretty-webapp".
    - Removed old favicon.ico file.
    - Added new favicon.png file.

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,7 +3,7 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
+    { title: "Pretty Shop" },
     { name: "description", content: "Welcome to React Router!" },
   ];
 }
